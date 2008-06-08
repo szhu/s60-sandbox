789b0d920cc27b4ce3142dd1eca3f6a54546bff3
Sean Zhu <interestinglythere@gmail.com>
2008-06-07 23:32:10 -0700

    Add Python/A/Restart.py

---
+++ Python/A/Restart.py
@@ -0,0 +1,6 @@
+import e32
+#e32.start_exe('Z:\System\Programs\AppRun.exe','Z:\System\Apps\Startup\Startup.app')
+e32.ao_sleep(0.1)
+#e32.start_exe('Z:\System\Programs\SplashScreen.exe')
+e32.ao_sleep(0.1)
+e32.start_exe('Z:\System\Programs\Starter.exe','Z:\System\Programs\SplashScreen.exe')
