b4c8f817adfe980b41095b16ca73b79901045867
Sean Zhu <interestinglythere@gmail.com>
2008-06-26 07:24:24 -0700

    Add Python/A/e32.py

---
+++ Python/A/e32.py
@@ -0,0 +1,10 @@
+import e32
+e32.start_exe(r'z:\system\programs\kmidrun.exe', r'MIDletSuiteAMSServer00000000*268437956*Compass*E:\System\Midp\Dana Peters\untrusted\Compass\4_091\Compass.jar*E:\System\Midp\Dana Peters\untrusted\Compass\4_091\Compass.jad*')
+
+#e32.start_exe(r'Z:\system\programs\AppRun.exe',r'Z:\System\Apps\CALENDAR\CALENDAR.app R Today')
+
+#e32.start_exe(r'Z:\system\programs\AppRun.exe',r'Z:\System\Apps\MCE\MCE.app RInbox')
+
+#e32.start_exe(r'Z:\system\programs\AppRun.exe',r'Z:\System\Apps\NOTEPAD\NOTEPAD.app R -1-1')
+
+#e32.start_exe(r'Z:\system\programs\AppRun.exe',r'E:\System\Apps\TextEdit\TextEdit.app E:\System\Apps\TextEdit\TextEdit.py')
